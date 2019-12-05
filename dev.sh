bef7d3a4f0f27af4b51969d27bdd4b70f2934987
Keloran <keloran@carpark.ninja>
2019-12-05 20:22:58 +0000

    updated

--- dev.sh
+++ dev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 STACK_NAME=nest
+BUILD_BUCKET=builds
 
 rm ${STACK_NAME}
 rm ${STACK_NAME}.zip
@@ -8,10 +9,20 @@ rm ${STACK_NAME}.zip
 GOOS=linux GOARCH=amd64 go build .
 zip ${STACK_NAME}.zip ${STACK_NAME}
 
+BUCKET_EXISTS=$(aws --region us-east-1 --endpoint-url http://localhost:4572 s3api list-buckets | jq '.Buckets[].Name//empty' | grep "${BUILD_BUCKET}")
+if [[ -z "${BUCKET_EXSISTS}" ]] || [[ "${BUCKET_EXISTS}" == "" ]]; then
+  aws --region us-east-1 --endpoint-url http://localhost:4572 s3api create-bucket --bucket ${BUILD_BUCKET}
+fi
+
+AUTH_ARN=unknown
+AUTH_EXISTS=$(aws --region us-east-1 --endpoint-url http://localhost:4574 lambda list-functions | jq '.Functions[].FunctionArn//empty' | grep "authorizer-lambda-dev")
+if [[ "${AUTH_EXISTS}" ]] || [[ "${AUTH_EXISTS}" != "" ]]; then	
+  AUTH_ARN=$(sed -e 's/^"//' -e 's/"$//' <<< ${AUTH_EXISTS})
+fi
+
 STACK_EXISTS=$(aws --region us-east-1 --endpoint-url http://localhost:4581 cloudformation list-stacks --stack-status-filter ROLLBACK_COMPLETE UPDATE_ROLLBACK_COMPLETE | jq '.StackSummaries[].StackName//empty' | grep "${STACK_NAME}")
-if [[ -z "$STACK_EXISTS" ]] || [[ "$STACK_EXISTS" == "" ]]; then
-  aws --region us-east-1 --endpoint-url http://localhost:4572 s3api create-bucket --bucket tester
-  aws --region us-east-1 --endpoint-url http://localhost:4572 s3 cp ./${STACK_NAME}.zip s3://tester/${STACK_NAME}.zip
+if [[ -z "${STACK_EXISTS}" ]] || [[ "${STACK_EXISTS}" == "" ]]; then
+  aws --region us-east-1 --endpoint-url http://localhost:4572 s3 cp ./${STACK_NAME}.zip s3://${BUILD_BUCKET}/${STACK_NAME}.zip
   aws --region us-east-1 --endpoint-url http://localhost:4580 route53 create-hosted-zone --name docker.devel --caller-reference devStuff
   aws --region us-east-1 --endpoint-url http://localhost:4581 cloudformation create-stack \
 	  --template-body file://cf.yaml \
@@ -21,13 +32,13 @@ if [[ -z "$STACK_EXISTS" ]] || [[ "$STACK_EXISTS" == "" ]]; then
 		  ParameterKey=ServiceName,ParameterValue=${STACK_NAME} \
 		  ParameterKey=BuildKey,ParameterValue=${STACK_NAME}.zip \
 		  ParameterKey=Environment,ParameterValue=dev \
-		  ParameterKey=BuildBucket,ParameterValue=tester \
-		  ParameterKey=AuthorizerARN,ParameterValue=tester \
+		  ParameterKey=BuildBucket,ParameterValue=${BUILD_BUCKET} \
+		  ParameterKey=AuthorizerARN,ParameterValue=${AUTH_ARN} \
 		  ParameterKey=CertificateARN,ParameterValue=tester \
 		  ParameterKey=DNSZoneName,ParameterValue=docker.devel \
 		  ParameterKey=DomainName,ParameterValue=api.docker.devel
 else
-  aws --region us-east-1 --endpoint-url http://localhost:4572 s3 cp ./${STACK_NAME}.zip s3://tester/${STACK_NAME}.zip
+  aws --region us-east-1 --endpoint-url http://localhost:4572 s3 cp ./${STACK_NAME}.zip s3://${BUILD_BUCKET}/${STACK_NAME}.zip
   aws --region us-east-1 --endpoint-url http://localhost:4581 cloudformation update-stack \
 	  --template-body file://cf.yaml \
 	  --stack-name ${STACK_NAME} \
@@ -36,9 +47,11 @@ else
 		  ParameterKey=ServiceName,ParameterValue=${STACK_NAME} \
 		  ParameterKey=BuildKey,ParameterValue=${STACK_NAME}.zip \
 		  ParameterKey=Environment,ParameterValue=dev \
-		  ParameterKey=BuildBucket,ParameterValue=tester \
-		  ParameterKey=AuthorizerARN,ParameterValue=tester \
+		  ParameterKey=BuildBucket,ParameterValue=${BUILD_BUCKET} \
+		  ParameterKey=AuthorizerARN,ParameterValue=${AUTH_ARN} \
 		  ParameterKey=CertificateARN,ParameterValue=tester \
 		  ParameterKey=DNSZoneName,ParameterValue=docker.devel \
 		  ParameterKey=DomainName,ParameterValue=api.docker.devel
 fi
+
+
